a66aa64df7e0bcdd753e89949c3722c3593e8b1c
pandawa1996 <chusnunnidhom@gmail.com>
2021-01-01 21:12:23 +0700

    Display List Keranjang

--- app/src/main/java/com/example/tokoandroid/fragment/KeranjangFragment.kt
+++ app/src/main/java/com/example/tokoandroid/fragment/KeranjangFragment.kt
@@ -5,56 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Delete
 import com.example.tokoandroid.R
+import com.example.tokoandroid.adapter.AdapterProduk
+import com.example.tokoandroid.room.MyDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class KeranjangFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var btnDelete: ImageView
+    lateinit var rvProduk: RecyclerView
+    lateinit var tvTotal: TextView
+    lateinit var btnBayar: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        val view: View=inflater.inflate(R.layout.fragment_keranjang, container, false)
+        init(view)
+
+        mainButton()
+        return view
+    }
+
+    private fun displayProduk() {
+
+        val myDb = MyDatabase.getInstance(requireActivity())
+        val listProduk = myDb!!.daoKeranjang().getAll() as ArrayList
+
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        rvProduk.adapter = AdapterProduk(requireActivity(), listProduk)
+        rvProduk.layoutManager = layoutManager
+    }
+
+    private fun mainButton() {
+        btnBayar.setOnClickListener{
+
+        }
+
+        btnDelete.setOnClickListener{
+
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_keranjang, container, false)
+    private fun init(view: View) {
+        btnDelete = view.findViewById(R.id.btn_delete)
+        rvProduk = view.findViewById(R.id.rv_produk)
+        tvTotal = view.findViewById(R.id.tv_total)
+        btnBayar = view.findViewById(R.id.btn_bayar)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onResume() {
+        displayProduk()
+        super.onResume()
     }
 }
